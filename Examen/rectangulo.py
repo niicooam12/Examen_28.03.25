1ac80cf692c0990915e640ddfc213f30e9c9820c
niicooam12 <nico.amador.montesinos@gmail.com>
2025-03-28 09:39:16 +0100

    Cambios clase rectangulo

---
+++ Examen/rectangulo.py
@@ -0,0 +1,13 @@
+class Rectangulo:
+    def __init__(self, coord1=Punto(), coord2=Punto()):
+        self.coord1 = coord1
+        self.coord2 = coord2
+
+    def calcular_base(self):
+        return abs(self.coord1.x - self.coord2.x)
+    
+    def calcular_altura(self):
+        return abs(self.coord1.y - self.coord2.y)
+    
+    def calcular_area(self):
+        return self.calcular_base() * self.calcular_altura()    
