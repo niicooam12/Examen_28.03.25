58cd6bd0c88548ac79cdf4c50c09eaa8c7d900ea
niicooam12 <nico.amador.montesinos@gmail.com>
2025-03-28 09:30:40 +0100

    Cambio clase puntos

---
+++ Examen/punto.py
@@ -0,0 +1,23 @@
+import math
+
+class Punto:
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+        
+    def __str__(self):
+        if self.x == 0 and self.y == 0:
+            return "Origen de coordenadas"
+        elif self.x == 0:
+            return "Estas sobre el eje Y"
+        elif self.y == 0:
+            return "Estas sobre el eje X"
+        elif self.x > 0 and self.y > 0:
+            return "Estas en el cuadrante 1"
+        elif self.x < 0 and self.y > 0:
+            return "Estas en el cuadrante 2"    
+        elif self.x < 0 and self.y < 0:
+            return "Estas en el cuadrante 3"
+        elif self.x > 0 and self.y < 0:
+            return "Estas en el cuadrante 4"   
+        
